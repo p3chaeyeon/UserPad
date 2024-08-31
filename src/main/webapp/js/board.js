14480c3cd5a820abd2baa8f02f371d6c26fcaa0a
p3chaeyeon <p3chaeyeon@gmail.com>
2024-08-31 16:22:55 +0900

    side-menu mouse hover

--- src/main/webapp/js/board.js
+++ src/main/webapp/js/board.js
@@ -1,4 +1,34 @@
+// board.js
 $(function() {
+	$('.menu-list').hover(
+	    function() {
+	        // 현재 항목 스타일 변경
+	        $(this).css({
+	            'color': '#F1AF23',
+	            'font-weight': 'bold'
+	        });
+
+	        // 다른 메뉴 항목들의 스타일 복구
+	        $('.menu-list').not(this).css({
+	            'color': 'white',
+	            'font-weight': 'normal'
+	        });
+	    },
+	    function() {
+	        // 현재 항목 스타일 원래대로 복구
+	        $(this).css({
+	            'color': 'white',
+	            'font-weight': 'normal'
+	        });
+
+	        // 항상 "글 목록" 항목은 강조 스타일로 유지
+	        $('a[href="board.html"]').find('p').css({
+	            'color': '#F1AF23',
+	            'font-weight': 'bold'
+	        });
+	    }
+	);
+		
     $.ajax({
         type: 'GET',
         url: '../jsp/board.jsp', // board.jsp를 호출하여 데이터를 가져옴
