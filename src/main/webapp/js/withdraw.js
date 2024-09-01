4d116b048399c591fdc57f1ca9d89075a3562ee8
p3chaeyeon <p3chaeyeon@gmail.com>
2024-09-01 22:17:19 +0900

    fix : withdraw 쿠키 삭제

--- src/main/webapp/js/withdraw.js
+++ src/main/webapp/js/withdraw.js
@@ -1,1 +1,12 @@
 // widhdraw.js
+
+
+// 탈퇴 버튼 클릭 시 쿠키 삭제 및 페이지 이동
+$('.withdrawBtn').on('click', function() {
+    // 쿠키 삭제
+	document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    
+    // 탈퇴 후 페이지 이동
+    window.location.href = '../index.html';
+});
